6dafaf391c88f7603107899c18ce7b6a4962c774
Eric.zhang <beefjava@gmail.com>
2020-01-31 21:36:13 +0800

    feat(logs): add logger & logs

--- server/app.js
+++ server/app.js
@@ -1,14 +1,18 @@
 const path = require('path');
+const fs = require('fs');
 const Koa = require('koa');
 const static = require('koa-static');
 const parameter = require('koa-parameter');
 const koaBody = require('koa-body');
+const logger = require('koa-logger');
+const morgan = require('koa-morgan');
 const error = require('koa-json-error');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
+const env = process.env.NODE_ENV || 'development';
 const routing = require('./routes');
 const config = require('../config');
 
@@ -39,7 +43,30 @@ app.use(
     multipart: true,
   }),
 );
-app.use(error());
+app.use(logger());
+// logs use morgan
+if (env !== 'production') {
+  app.use(morgan('dev'));
+} else {
+  // production mode
+  const logsFileName = path.join(__dirname, '../logs', 'access.log');
+  const writeStream = fs.createWriteStream(logsFileName, {
+    flags: 'a',
+  });
+  app.use(
+    morgan('combined', {
+      stream: writeStream, // default
+    }),
+  );
+}
+// format error to JSON
+app.use(
+  error({
+    // avoid showing the stacktrace in 'production' env
+    postFormat: (err, { stack, ...rest }) =>
+      env === 'production' ? rest : { stack, ...rest },
+  }),
+);
 
 routing(app);
 
